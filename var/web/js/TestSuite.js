d4e9dccafafebef85a3e88857e8d83e40fc8e3f0
Visu Patlolla <visu.patlolla@agilex.com>
2012-03-10 16:59:08 -0500

    created a number of tests and should be able to support sending it over the wire; still need to add code to send over the wire

--- var/web/js/TestSuite.js
+++ var/web/js/TestSuite.js
@@ -1,3 +1,45 @@
+var client = {
+	sendCalled: false,
+	
+	send: function(client, task) {
+		this.sendCalled = true;
+	}
+}
+
 test("first test within module", function() {
     ok( true, "all pass" );
 });
+
+test("testing something", function() {
+	localStorage.setItem("hi", "hi")
+	var actual = localStorage.getItem("hi");
+	ok("hi" == actual, "testing set and get")
+})
+
+test("Mock Client Send is being called", function() {
+	send_task(client, new Task("a", "b"));
+	ok(client.sendCalled == true, "Good work");
+})
+
+test("Create a new Task", function() {
+	var t = new Task("title", "state");
+	ok(t.title == "title", "Title Verified");
+	ok(t.state == "state", "State Verified");
+	ok(t.id != "", "Id properly set");
+})
+
+test("UUID not reused", function() {
+	var t = new Task("title1", "state1");
+	var t2 = new Task("title2", "state2");
+	ok(t.id != t2.id, "Ids function properly");
+})
+
+test("Ensure Local Storage is updated from callback", function() {
+	var taskToReceive = new Task("callback title", "open")
+	receive_task_callback(JSON.stringify(taskToReceive))
+	var fromStorage = localStorage.getItem(taskToReceive.id)
+	var taskObject = JSON.parse(fromStorage)
+	ok(taskToReceive.id == taskObject.id, "Id is good")
+	ok(taskToReceive.title == taskObject.title, "title is good")
+	ok(taskToReceive.state == taskObject.state, "state is good")
+})
--- var/web/js/task.js
+++ var/web/js/task.js
@@ -1,5 +1,67 @@
 var global_store
 
+var UUID = function b(a){
+	return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)
+}
+
+var Task = function(title, state) {
+	this.id = UUID();
+	this.title = title;
+	this.state = state;
+}
+
+var client = Stomple.create_client({
+	  	url: "ws://" + window.location.hostname + ":61614/stomp",
+	  	destination: "jms.topic.chat",
+		login: "guest",
+        passcode: "guest",
+
+		socketOpen: function() {
+			console.log("socket opened");
+		},
+        
+        socketMessage: function(msg) {
+			console.log("socket msg");
+			console.log(msg);
+	    },
+        
+        socketClose: function(e) {
+			console.log("socket close");
+			console.log(e);
+	    },
+        
+		socketError: function() {
+			console.log("socket error");
+			console.log(client.websocket);
+			return false;
+		}
+	  
+  });
+  var msgHandler = {
+		fn: function(task) {
+			upateLocalStorage(task);
+		  	var c = document.getElementById('output');
+			c.innerHTML = c.innerHTML + '<br>' + msg.body;
+		}
+  };
+	var updateLocalStorage = {
+		fn: function(task) {
+			// store in the local storage database
+		}
+	}
+  client.subscribe({
+	handler: msgHandler.fn,
+	thisObj: msgHandler,
+	
+	success: function() {	//did subscription succeed?
+		console.log("sub ok..");
+	},
+	failure: function(reason) {		//did subscription fail?
+		console.log(reason);
+	}
+  });
+
+
 function create_task(text){
     // add to local storage
     //  send_update_task()
@@ -10,14 +72,18 @@ function change_task_state(task, state){
     // send_update_task()
 }
 
-function send_update_task(task){
-    // serialize task and send it
+function send_task(client, task){
+	task = JSON.stringify(task);
+
+	client.send({
+		body: client.session+": "+ task,
+	});
     // Send the serialized task to the jms destination/broker
 }
 
-function update_task_callback(){
-    // callback that's called when the broker sends us a new task
-    // unserialize && update local storage when received
+function receive_task_callback(message){
+    var task = JSON.parse(message);
+	localStorage[task.id] = JSON.stringify(task);
 }
 
 function update_viewer(){
